51bae348ad05175b3c2a623a6400ccc62dc5ec5b
nakniner <nak_niner@hotmail.com>
2018-07-16 15:29:20 +0900

    add EchoClient

--- src/EchoServerHandler.java
+++ src/EchoServerHandler.java
@@ -1,9 +1,20 @@
 import io.netty.channel.*;
+import io.netty.buffer.*;
+import java.nio.charset.*;
 
-public class EchoServerHandler extends SimpleChannelInboundHandler<Object> {
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
     @Override
-    public void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
         //do nothing.
+        String readMessage = ((ByteBuf)msg).toString(Charset.defaultCharset());
+
+        System.out.println("수신한 문자열 [" + readMessage + "]");
+
+        ctx.write(msg);
+    }
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
     }
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {

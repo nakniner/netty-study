41ca62c4209ab359747fc8c64eec6255983a0e9d
nakniner <nak_niner@hotmail.com>
2018-07-16 11:07:04 +0900

    the first commit

---
+++ src/EchoServerHandler.java
@@ -0,0 +1,13 @@
+import io.netty.channel.*;
+
+public class EchoServerHandler extends SimpleChannelInboundHandler<Object> {
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+        //do nothing.
+    }
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
